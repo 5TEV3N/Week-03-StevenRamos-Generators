7899fdf4d2d675bf3d473bff31c560f4373cb750
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-09-18 20:48:01 -0400

    First Commit

---
+++ Assets/Scripts/_Gameplan.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gameplan : MonoBehaviour
+{
+    /*
+        Gameplan
+        Name of game: Generating Meaning (Art Installation Generator)
+        Summary:
+            - Generate random objects at the center of the room (Possibly in random angels)
+            - Layered audio
+            - Reloading Scene -> Random Objects on the scene, different audio plays
+    */
+}
