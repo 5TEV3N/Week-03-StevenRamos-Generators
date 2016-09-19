570927b763b0e4c125e04524182952fa40f14773
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-09-19 18:49:01 -0400

    Implemented the AudioSource > Piano vers

--- Assets/Credits.cs
+++ Assets/Credits.cs
@@ -9,9 +9,13 @@ public class NewBehaviourScript : MonoBehaviour
      * - Unity Documentation by Various
      * - week-03-generators by Six
      * 
-     * Music
-     * - 
-     * - 
-     * 
+     * Music 
+       - Shady Dave:
+     * - https://www.freesound.org/people/ShadyDave/sounds/277447/
+     * - https://www.freesound.org/people/ShadyDave/sounds/333811/
+     * - https://www.freesound.org/people/ShadyDave/sounds/325611/
+       - https://www.freesound.org/people/ShadyDave/sounds/325647/
+       -
+       - https://www.freesound.org/people/waveplay/sounds/235835/
     */
 }
--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -3,5 +3,19 @@ using System.Collections;
 
 public class AudioManager : MonoBehaviour
 {
+    public AudioSource[] Piano;
+    public AudioSource[] Beats;
+    public AudioSource[] Spazaz; //Lack of a better term
 
+    void Awake()
+    {
+        //PIANO SECTION
+        AudioSource pianoArray = Piano[Random.Range(0, Piano.Length)];
+        AudioSource pianoSpawn = Instantiate (pianoArray);
+
+        //BEATS SECTION
+
+
+        //SPAZAZ SECTION
+    }
 }
